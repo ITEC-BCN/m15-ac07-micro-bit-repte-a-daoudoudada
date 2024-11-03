dafeefc03afa254ce421a46d4e5e4ea5ddd723a8
Daouud <146826985+daoudoudada@users.noreply.github.com>
2024-11-03 15:09:33 +0100

    toda lalogica

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+eleccionJugador = 0
+eleccionMaquina = 0
+
 def on_button_pressed_a():
+    global eleccionJugador
+    eleccionJugador = 1
     basic.show_leds("""
         . . . . .
         . # # # .
@@ -8,20 +13,43 @@ def on_button_pressed_a():
         """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_gesture_shake():
+    global eleccionMaquina
+    eleccionMaquina = randint(1, 3)
+    if eleccionMaquina == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif eleccionMaquina == 2:
+        basic.show_leds("""
+            . # # # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # # # .
+            """)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def on_button_pressed_ab():
+    global eleccionJugador
+    eleccionJugador = 3
     basic.show_icon(IconNames.SCISSORS)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
+    global eleccionJugador
+    eleccionJugador = 2
     basic.show_leds("""
+        . . . . .
         . # # # .
-        . # . # .
         . # . # .
         . # . # .
         . # # # .
         """)
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,26 @@
+let eleccionJugador = 0
+let eleccionMaquina = 0
+function determinarGanador () {
+    if (eleccionJugador == eleccionMaquina) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    } else if (eleccionJugador == 1 && eleccionMaquina == 3) {
+        basic.showIcon(IconNames.Yes)
+    } else if (eleccionJugador == 3 && eleccionMaquina == 2) {
+        basic.showIcon(IconNames.Yes)
+    } else if (eleccionJugador == 2 && eleccionMaquina == 1) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+}
 input.onButtonPressed(Button.A, function () {
+    eleccionJugador = 1
     basic.showLeds(`
         . . . . .
         . # # # .
@@ -6,11 +28,37 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . . . . .
         `)
+    determinarGanador()
+})
+input.onGesture(Gesture.Shake, function () {
+    eleccionMaquina = randint(1, 3)
+    if (eleccionMaquina == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (eleccionMaquina == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # # # .
+            `)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
+    eleccionJugador = 3
     basic.showIcon(IconNames.Scissors)
+    determinarGanador()
 })
 input.onButtonPressed(Button.B, function () {
+    eleccionJugador = 2
     basic.showLeds(`
         . # # # .
         . # . # .
@@ -18,4 +66,5 @@ input.onButtonPressed(Button.B, function () {
         . # . # .
         . # # # .
         `)
+    determinarGanador()
 })
